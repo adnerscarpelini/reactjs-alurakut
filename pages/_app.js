05f7fbb8ff6a4c1ebdeb72cac2f838911116b451
Adner Scarpelini <adner.scarpelini@gmail.com>
2021-07-12 19:06:49 -0300

    Aula 01
    Aula 01 - Components com React, Styled Components e NextJS | Imersão React

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,37 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+
+  //Reset CSS para navegadores antigos
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box
+  }
+
+
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #D9E6F6
   }
+
+  /* A tag Next é a inicial gerada automaticamente */
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  /* Reset para deixar imagens responsivas por padrão */
+  img{
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+
 `
 
 const theme = {
